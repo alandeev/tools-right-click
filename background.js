5dacf5776b7b093f9ae22d56db4b49242802deac
alandev2 <alanworking1@gmail.com>
2021-06-06 00:10:00 -0300

    feat: add pages and refactor

--- background.js
+++ background.js
@@ -1,58 +1,17 @@
-const tabs = [];
+const commands = [];
 
-const baseURL = 'https://translate.google.com'
+try{
+  importScripts('/path/translator.js')
 
-const languageConfig = {
-  from: 'en',
-  to: 'pt'
-}
-
-const findTabByUrl = () => {
-  const tab = tabs.find(tab => {
-    const tabUrl = tab.pendingUrl
-    if(!tabUrl) return false;
-
-    return tabUrl.match(baseURL)
-  })
-
-  return tab;
-};
-
-const openTranslator = (text, tabId) => {
-  if(!text) return;
-
-  const url = `${baseURL}?sl=${languageConfig.from}&tl=${languageConfig.to}&text=${encodeURI(text)}&op=translate`
-
-  tabId ?
-  ( chrome.tabs.update(tabId, { url, active: true }) ) :
-  ( window.open(url) );
-}
-
-const handleTranslate = ({ selectionText }) => {
-  const tab = findTabByUrl(baseURL)
-  const tabId = tab ? tab.id : null;
-  openTranslator(selectionText, tabId)
-}
-
-const Chrome = {
-  contextMenuDetails: {
-    "title": "Traduzir seleção",
-    "contexts": ["selection"],
-    "onclick": handleTranslate
-  },
-  removeEventCallback: (tabId) => {
-    const deletedIndex = tabs.findIndex(c => c.id === tabId);
-
-    tabs.splice(deletedIndex, 1)
-  },
-  createEventCallback: (tab) => {
-    tab.id && chrome.tabs.get(tab.id, (tab) => {
-      tabs.push(tab)
-    })
-  }
+  commands.push(ChromeTranslator)
+}catch(error) {
+  console.log(error)
 }
 
 // Configuring Events
-chrome.contextMenus.create(Chrome.contextMenuDetails)
-chrome.tabs.onRemoved.addListener(Chrome.removeEventCallback);
-chrome.tabs.onCreated.addListener(Chrome.createEventCallback);
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  const command = commands.find(cmd => cmd.id === info.menuItemId);
+  if (!command) return
+
+  command.execute(info)
+});
---
+++ path/translator.js
@@ -0,0 +1,58 @@
+const tabs = [];
+const baseURLTranslator = 'https://translate.google.com';
+
+const languageConfigTranslator = {
+  from: 'en',
+  to: 'pt'
+}
+
+const findTabByUrlTranslator = () => {
+  const tab = tabs.find(tab => {
+    const tabUrl = tab.pendingUrl
+    if(!tabUrl) return false;
+
+    return tabUrl.match(baseURLTranslator)
+  })
+
+  return tab;
+}
+
+const handlerTranslator = ({ selectionText }) => {
+  if(!selectionText) return;
+
+  const tab = findTabByUrlTranslator(baseURLTranslator)
+  const tabId = tab ? tab.id : null;
+
+  const url = `${baseURLTranslator}?sl=${languageConfigTranslator.from}&tl=${languageConfigTranslator.to}&text=${encodeURI(selectionText)}&op=translate`
+
+  tabId ?
+  ( chrome.tabs.update(tabId, { url, active: true }) ) :
+  (   chrome.tabs.create({ url, active: true }) )
+}
+
+const removeEventCallBackTranslator = (tabId) => {
+  const deletedIndex = tabs.findIndex(c => c.id === tabId);
+
+  tabs.splice(deletedIndex, 1);
+}
+
+const createEventCallbackTranslator = (tab) => {
+  tab.id && chrome.tabs.get(tab.id, (tab) => {
+    tabs.push(tab);
+  })
+}
+
+
+const ChromeTranslator = {
+  id: 'translator-command',
+  execute: handlerTranslator
+}
+
+chrome.contextMenus.create({
+  id: 'translator-command',
+  title: 'Traduzir texto selecionado',
+  contexts: ['selection']
+});
+
+chrome.tabs.onRemoved.addListener(removeEventCallBackTranslator);
+chrome.tabs.onCreated.addListener(createEventCallbackTranslator);
